1edae8c3c587ba7b2c8b301f3527b2de2a71c79a
AlexMasonAM <alexmmason@gmail.com>
2015-01-14 10:43:21 -0800

    added SignUp page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
+
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+  field :password_digest, type: String
+
+  has_secure_password #tells rails to have a password_digest
+
+  validates :password, confirmation: true #standard for WDI not in real world
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'ingredients#index'
 
+  get '/signup' => 'users#new'
+  post '/user' => 'users#create'
+
+  resources :users
   resources :ingredients
   resources :employees
 
